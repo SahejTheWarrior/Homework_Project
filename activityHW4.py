f75905525b2e8d0c59534c1ed22c4237271e1f34
SahejTheWarrior <sahejburande@gmail.com>
2025-10-29 17:24:21 +0000

    Add files via upload

---
+++ activityHW4.py
@@ -0,0 +1,11 @@
+#your customised ride
+print("select your number")
+number=int(input(""))
+print("select a power below 3")
+power=int(input(""))
+if(power==1):
+    print("The awnser is "(number*power))
+elif(power==2):
+    print("The awnser is "(number*power))
+else:
+    print("wrong choice!!!")
