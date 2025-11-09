828838f5e5898f7dffba16d437e6353abb7c627d
SahejTheWarrior <sahejburande@gmail.com>
2025-11-09 11:44:49 +0000

    Add files via upload

---
+++ activityHW11.py
@@ -0,0 +1,8 @@
+# Program to check if a character is an alphabet
+
+ch = input("Enter a character: ")
+
+if ch.isalpha():
+    print(f"'{ch}' is an alphabet.")
+else:
+    print(f"'{ch}' is NOT an alphabet.")
