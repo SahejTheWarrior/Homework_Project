ca60087f86172d5a9858cb48b575fe48979933f0
SahejTheWarrior <sahejburande@gmail.com>
2025-11-09 11:41:05 +0000

    Add files via upload

---
+++ activityHW9.py
@@ -0,0 +1,16 @@
+# Initial values
+a = 10
+b = 20
+c = 30
+
+print("Before swapping:")
+print("a =", a, "b =", b, "c =", c)
+
+# Swapping using a temporary variable
+temp = a
+a = b
+b = c
+c = temp
+
+print("\nAfter swapping:")
+print("a =", a, "b =", b, "c =", c)
