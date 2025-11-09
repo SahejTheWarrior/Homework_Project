9b6adc563cb339e83c697d38c4768994071f856c
SahejTheWarrior <sahejburande@gmail.com>
2025-11-09 11:43:00 +0000

    Add files via upload

---
+++ activityHW10.py
@@ -0,0 +1,9 @@
+# Program to check ASCII code of alphabets
+
+# Ask the user for a character
+ch = input("Enter an alphabet: ")
+
+# Use the ord() function to get ASCII code
+ascii_code = ord(ch)
+
+print(f"The ASCII code of '{ch}' is {ascii_code}")
