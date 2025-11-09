26185c3d499842e685230955e7713bb50f72fa3f
SahejTheWarrior <sahejburande@gmail.com>
2025-11-09 11:48:35 +0000

    Add files via upload

---
+++ activityHW13.py
@@ -0,0 +1,9 @@
+# Program to find the square root of a number
+
+import math
+
+num = float(input("Enter a number: "))
+
+sqrt_value = math.sqrt(num)
+
+print(f"The square root of {num} is {sqrt_value}")
